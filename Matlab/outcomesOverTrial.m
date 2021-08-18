c25f053811a39890fd7c4a23903cae03768effca
John Maunsell <maunsell@uchicago.edu>
2021-08-17 20:50:09 -0500

    Changed Matlab outcomesOverTrial.m to use histcount rather than hist.  We can do this now that all the machines are using modern Matlab.

--- Matlab/outcomesOverTrial.m
+++ Matlab/outcomesOverTrial.m
@@ -21,20 +21,15 @@ function outcomesOverTrial(dParams, plotIndex, file, trials, indices, trialStruc
      missTimes = -10000;
   end
   releaseTimes = [(hitTimes + hitRTs), (faTimes + faRTs)];
-
-  % histc uses edges in a ridiculous way.  The first and last edge limit the range, but the bin edges used in between
-  % aren't the remaining values.  They are the spots halfway between the remaining values.  Absurd.  This is presumably
-  % why Matlab recommends histcounts.  But we don't have that function on all our versions of Matlab.  We set the bin
-  % edges up so that there are 2 small bins on either side of the range, which we will merge at the end of the counting.
   subplot(dParams.plotLayout{:}, plotIndex);
   cla;
   hold on;
-  trialBins = max(10, length(trials) / 20) ;                   	% 10 or more bins
+  trialBins = max(10, length(trials) / 20);                   	% 10 or more bins
   set(gca, 'XLim', [1 trialBins]);
   if isfield(file, 'responseLimitMS')
-  timeRangeMS = file.preStimMaxMS + file.responseLimitMS;
-  minStimXPos = file.preStimMinMS / timeRangeMS * (trialBins - 1) + 1;
-  maxStimXPos = file.preStimMaxMS / timeRangeMS * (trialBins - 1) + 1;
+    timeRangeMS = max(file.preStimMaxMS + file.responseLimitMS, 1);
+    minStimXPos = file.preStimMinMS / timeRangeMS * (trialBins - 1) + 1;
+    maxStimXPos = file.preStimMaxMS / timeRangeMS * (trialBins - 1) + 1;
     if minStimXPos >= maxStimXPos
         if maxStimXPos >= trialBins
             maxStimXPos = trialBins;
@@ -63,25 +58,32 @@ function outcomesOverTrial(dParams, plotIndex, file, trials, indices, trialStruc
     set(gca,'XTick', [1, trialBins]);
     set(gca,'XTickLabel',{'0', sprintf('%d', timeRangeMS)});  
   end
-  binWidth = timeRangeMS / trialBins;                            % binWidth in MS
-  edges = [0.0, linspace(0.5 * binWidth, timeRangeMS - 0.5 * binWidth, trialBins), timeRangeMS];
-  counts = hist(hitTimes, edges);
-  nHits = [counts(1) + counts(2), counts(3:end-2), counts(end-1) + counts(end)];
-  counts = hist(faTimes, edges);
-  nFAs = [counts(1) + counts(2), counts(3:end-2), counts(end-1) + counts(end)];
-  counts = hist(missTimes, edges);
-  nMisses = [counts(1) + counts(2), counts(3:end-2), counts(end-1) + counts(end)];
-  nTotal = nHits + nFAs + nMisses;
-  counts = hist(releaseTimes, edges);
-  nReleases = [counts(1) + counts(2), counts(3:end-2), counts(end-1) + counts(end)];
-  if sum(nTotal) < 1                                     % no counts yet?
+%   binWidth = timeRangeMS / trialBins;                            % binWidth in MS
+%   edges = [0.0, linspace(0.5 * binWidth, timeRangeMS - 0.5 * binWidth, trialBins), timeRangeMS];
+%   counts = hist(hitTimes, edges);
+%   nHits = [counts(1) + counts(2), counts(3:end-2), counts(end-1) + counts(end)];
+%   counts = hist(faTimes, edges);
+%   nFAs = [counts(1) + counts(2), counts(3:end-2), counts(end-1) + counts(end)];
+%   counts = hist(missTimes, edges);
+%   nMisses = [counts(1) + counts(2), counts(3:end-2), counts(end-1) + counts(end)];
+%   nTotal = nHits + nFAs + nMisses;
+%   counts = hist(releaseTimes, edges);
+%   nReleases = [counts(1) + counts(2), counts(3:end-2), counts(end-1) + counts(end)];
+  
+  edges = linspace(0, timeRangeMS, trialBins);
+  hitCounts = histcounts(hitTimes, edges);
+  faCounts = histcounts(faTimes, edges);
+  missCounts = histcounts(missTimes, edges);
+  nTotal = hitCounts + faCounts + missCounts;
+  if isempty(find(nTotal > 0, true, 'first'))           % no counts yet?
      return;
   end
+  earlyCounts = histcounts(releaseTimes, edges);
 
-  pHits = nHits ./ nTotal;                               % proportions of hits, FAs and misses
-  pFAs = nFAs ./ nTotal;
-  pMisses = nMisses ./ nTotal;
-  pReleases = nReleases ./ (max(nReleases) * 1.25);
+  pHits = hitCounts ./ nTotal;                          % proportions of hits, FAs and misses
+  pFAs = faCounts ./ nTotal;
+  pMisses = missCounts ./ nTotal;
+  pEarly = earlyCounts ./ (max(earlyCounts) * 1.25);
   xlabel('Time of stimulus onset (ms)');
   ylabel('Proportion of trials');
   set(gca, 'YLim', [0 1]);
@@ -91,7 +93,7 @@ function outcomesOverTrial(dParams, plotIndex, file, trials, indices, trialStruc
   plot(pHits, 'color', [0.0, 0.7, 0.0], 'lineWidth', 1);
   plot(pFAs, 'color', [0.9, 0.0, 0.0], 'lineWidth', 1);
   plot(pMisses, 'color', [0.6, 0.4, 0.2], 'lineWidth', 1);
-  plot(pReleases, 'color', [0.6, 0.6, 0.6], 'lineWidth', 1);
+  plot(pEarly, 'color', [0.6, 0.6, 0.6], 'lineWidth', 1);
   if maxStimXPos > minStimXPos
     plot([minStimXPos, minStimXPos], [0, 1], 'k:');
   end
